d0b8bc044e3077ee0f8a82702d0a55682cb70b4c
RickySChai <rsc1401@uw.edu>
2016-12-18 12:00:55 -1000

    going back and forth

--- inG.js
+++ inG.js
@@ -1,40 +1,41 @@
 (function() {
 	"use strict";
 
-   var access_token = window.location.href.split("=")[1]; // 
-   console.log(access_token);
+   var access_token;
 
    window.onload = function() {
       // gets a users access token, stores to access_token      
       document.getElementById("search").onclick = get_data;
+      access_token = window.location.href.split("=")[1]; // 
+      console.log(access_token);
    };
 
-   // function get_data() {
-   //    var request = new XMLHttpRequest();
-   //    request.onload = fetchInfo;
-   //    request.open("GET", "https://api.instagram.com/v1/users/self/?access_token=" + access_token, 
-   //             true);
-   //    request.send();
-   // }
+   function get_data() {
+      var request = new XMLHttpRequest();
+      request.onload = fetchInfo;
+      request.open("GET", "https://api.instagram.com/v1/users/self/?access_token=" + access_token, 
+               true);
+      request.send();
+   }
 
-   // function fetchInfo() {
-   //    var data = JSON.parse(this.responseText);
-   //    console.log("hello");
-   // }
+   function fetchInfo() {
+      var data = JSON.parse(this.responseText);
+      console.log("hello");
+   }
 
    //gets users data (username, bio, follower counts, media, id, full name, profile pic)
-   function get_data() {
-      $.ajax ({  
-         type: "GET",
-         dataType: "jsonp",
-         cache: false,
-         url: "https://api.instagram.com/v1/users/self/?access_token=" + access_token,
-         success: function(data) {
-                                                             // EVERYTHING WORKS UP UNTIL HERE
-            var data_P = JSON.parse(data.responseText);
-            console.log(data_P);
+   // function get_data() {
+   //    $.ajax ({  
+   //       type: "GET",
+   //       dataType: "jsonp",
+   //       cache: false,
+   //       url: "https://api.instagram.com/v1/users/self/?access_token=" + access_token,
+   //       success: function(data) {
+   //                                                           // EVERYTHING WORKS UP UNTIL HERE
+   //          var data_P = JSON.parse(data.responseText);
+   //          console.log(data_P);
            
-         }
-      });
-   }
+   //       }
+   //    });
+   // }
 })();
