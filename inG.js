e86c3b1730eec04136ffb9633821ca0a55b6b75c
RickySChai <rsc1401@uw.edu>
2016-12-18 12:11:16 -1000

    ahhhhh

--- inG.js
+++ inG.js
@@ -8,34 +8,36 @@
       document.getElementById("search").onclick = get_data;
    };
 
+   // function get_data() {
+   //    access_token = window.location.href.split("=")[1]; 
+   //    console.log(access_token);
+   //    var request = new XMLHttpRequest();
+   //    request.onload = fetchInfo;
+   //    request.open("GET", "https://api.instagram.com/v1/users/self/?access_token=" + access_token, 
+   //             true);
+   //    request.send();
+   // }
+
+   // function fetchInfo() {
+   //    var data = JSON.parse(this.responseText);
+   //    console.log("hello");
+   // }
+
+   //gets users data (username, bio, follower counts, media, id, full name, profile pic)
    function get_data() {
       access_token = window.location.href.split("=")[1]; 
       console.log(access_token);
-      var request = new XMLHttpRequest();
-      request.onload = fetchInfo;
-      request.open("GET", "https://api.instagram.com/v1/users/self/?access_token=" + access_token, 
-               true);
-      request.send();
-   }
-
-   function fetchInfo() {
-      var data = JSON.parse(this.responseText);
-      console.log("hello");
-   }
-
-   //gets users data (username, bio, follower counts, media, id, full name, profile pic)
-   // function get_data() {
-   //    $.ajax ({  
-   //       type: "GET",
-   //       dataType: "jsonp",
-   //       cache: false,
-   //       url: "https://api.instagram.com/v1/users/self/?access_token=" + access_token,
-   //       success: function(data) {
-   //                                                           // EVERYTHING WORKS UP UNTIL HERE
-   //          var data_P = JSON.parse(data.responseText);
-   //          console.log(data_P);
+      $.ajax ({  
+         type: "GET",
+         dataType: "jsonp",
+         cache: false,
+         url: "https://api.instagram.com/v1/users/self/?access_token=" + access_token,
+         success: function(data) {
+                                                             // EVERYTHING WORKS UP UNTIL HERE
+            var data_P = JSON.parse(data.responseText);
+            console.log(data_P);
            
-   //       }
-   //    });
-   // }
+         }
+      });
+   }
 })();
