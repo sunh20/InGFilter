b65ab7e906164d485c05853b85809d7a3ea55287
RickySChai <rsc1401@uw.edu>
2016-12-20 08:30:59 -1000

    testing separation of js

--- inG.js
+++ inG.js
@@ -5,40 +5,60 @@
 
    window.onload = function() {
       // gets a users access token, stores to access_token      
-      document.getElementById("search").onclick = get_data;
+      document.getElementById("load").onclick = getUserData;
+      document.getElementById("recent").onclick = getRecentMedia;
    };
 
-   // function get_data() {
-   //    access_token = window.location.href.split("=")[1]; 
-   //    console.log(access_token);
-   //    var request = new XMLHttpRequest();
-   //    request.onload = fetchInfo;
-   //    request.open("GET", "https://api.instagram.com/v1/users/self/?access_token=" + access_token, 
-   //             true);
-   //    request.send();
-   // }
-
-   // function fetchInfo() {
-   //    var data = JSON.parse(this.responseText);
-   //    console.log("hello");
-   // }
-
-   //gets users data (username, bio, follower counts, media, id, full name, profile pic)
-   function get_data() {
-      access_token = window.location.href.split("=")[1]; 
-      console.log(access_token);
+      // gets a users access token, stores to access_token      
+   var access_token = window.location.href.split("=")[1];
+   // get recent media only want this to run once
+   var hasRun = false;
+   if (access_token != null) {
+      console.log("Authentication complete");  
+      console.log(access_token); // for debugging
+   }
+      
+   // gets users data (username, bio, follower counts, media, id, full name, profile pic)
+   function getUserData() {
+      this.style.display = 'none';
       $.ajax ({  
          type: "GET",
          dataType: "jsonp",
          cache: false,
-         crossDomain : true,
          url: "https://api.instagram.com/v1/users/self/?access_token=" + access_token,
          success: function(data) {
-                                                             // EVERYTHING WORKS UP UNTIL HERE
-            var data_P = JSON.parse(data.responseText);
-            console.log(data_P);
-           
+            console.log("sucessfully retrived user data");
+            $('#name').text(data.data.username);
+            $('#bio').text(data.data.bio);
+            $('#realName').text(data.data.full_name);
+            $("#pic").append("<div'><img src='" + data.data.profile_picture + "'></img></a></div>");
          }
       });
+      return null;
+   }
+
+   function getRecentMedia() {
+      if (!hasRun) {
+         $.ajax ({  
+            type: "GET",
+            dataType: "jsonp",
+            cache: false,
+            url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + access_token,
+            success: function(data) {
+               console.log("sucessfully retrived media");
+              // var totalLength = data.data.length;
+               var totalLength = 10;
+               for (var i = 0; i < totalLength; i++) {
+                  if (data.data[i].type === "video") {
+                     $("#tenpics").append("<div class='media'><a target='_blank' href='" + data.data[i].link + "'><video controls loop autoplay class='media' src='" + data.data[i].videos.low_resolution.url + "'></video></a></div>");
+                  } else { 
+                     $("#tenpics").append("<div class='media'><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></div>");
+                  }
+               }
+            }
+         });
+      }
+      hasRun = true;
+      return null;
    }
 })();
