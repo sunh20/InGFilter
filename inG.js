750b7e3c8231aff95e6a75ec3ef70b74a6b2084a
sunh20 <sunh20@uw.edu>
2017-01-20 12:03:50 -0800

    import charts, try toggle display

--- inG.js
+++ inG.js
@@ -16,8 +16,9 @@
       console.log(access_token); // for debugging
    }
 
-   // get recent media, only want this to run once
-   var hasRun = false;
+   // hasRun to toggle display
+   var hasRunRecent = false;
+   var hasRunTop = false;
 
    // 2D array stores media data (link, likes, image or video, lowres.url)
    var media = [];
@@ -25,6 +26,10 @@
    // number of recent/top photos to display. User can change
    var recent = 5;
    var top = 3;
+
+   // import charts
+   var Chart = require('chart.js')
+   var myChart = new Chart({...})
 
    // gets users data (username, bio, follower counts, media, id, full name, profile pic)
    function getUserData() {
@@ -53,76 +58,88 @@
 
    // gets all photos from user, creates 2D array of media (link, likes, image or video, lowres.url)
    function getMedia() {
-      if (!hasRun) {
-         $.ajax ({  
-            type: "GET",
-            dataType: "jsonp",
-            cache: false,
-            url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + access_token,
-            success: function(data) {
-               console.log("sucessfully retrived media");
-               var totalLength = data.data.length;
-               for (var i = 0; i < totalLength; i++) {
-                  if (data.data[i].type === "video") {
-                     media.push([data.data[i].link, data.data[i].likes["count"], "video", data.data[i].videos.low_resolution.url]);                 
-                  } else { 
-                     media.push([data.data[i].link, data.data[i].likes["count"], "image", data.data[i].images.low_resolution.url]);
-                  }
+      $.ajax ({  
+         type: "GET",
+         dataType: "jsonp",
+         cache: false,
+         url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + access_token,
+         success: function(data) {
+            console.log("sucessfully retrived media");
+            var totalLength = data.data.length;
+            for (var i = 0; i < totalLength; i++) {
+               if (data.data[i].type === "video") {
+                  media.push([data.data[i].link, data.data[i].likes["count"], "video", data.data[i].videos.low_resolution.url]);                 
+               } else { 
+                  media.push([data.data[i].link, data.data[i].likes["count"], "image", data.data[i].images.low_resolution.url]);
                }
-               // console.log(media); // debugging, everything is fine up to here
             }
-         });
-      }
-      hasRun = true;
+         }
+      });
       document.getElementById("loadMedia").style.display = "none";
       return media;
 
    }
    
-   // displays recent photos
-   function showRecent() {
-      // console.log(media); IT FINALLY WORKED OMG
-      
-      for (var i = 0; i < recent; i++) {
-         if (media[i][2] === "video") {
-            $("#recentpics").append("<div class='media'><a target='_blank' href='" + media[i][0] + "'><video controls loop autoplay class='media' src='" + media[i][3] + "'></video></a></div>");
-         } else { 
-            $("#recentpics").append("<div class='media'><a target='_blank' href='" + media[i][0] + "'><img src='" + media[i][3] + "'></img></a></div>");
+   // toggles display of recent photos
+   function showRecent() {      
+      if (!hasRunRecent) {
+         for (var i = 0; i < recent; i++) {
+            if (media[i][2] === "video") {
+               $("#recentpics").append("<div class='media'><a target='_blank' href='" + media[i][0] + "'><video controls loop autoplay class='media' src='" + media[i][3] + "'></video></a></div>");
+            } else { 
+               $("#recentpics").append("<div class='media'><a target='_blank' href='" + media[i][0] + "'><img src='" + media[i][3] + "'></img></a></div>");
+            }
          }
+         hasRunRecent = true;
+      } else {
+         toggleDisplay("recentpics");
       }
-      document.getElementById("showRecent").style.display = "none";
       return null;      
    }
    
    // displays top photos
    function showTop() {
-      // create new 2D array with index # and likes
-      var items = []
-      for (var i = 0; i < media.length; i++) {
-         items.push([i, media[i][1]]);
-      }
-      console.log(items);
-
-      // sort array by likes
-      items.sort(function(first, second) {
-          return second[1] - first[1];
-      });
-      console.log(items);
-
-      items = items.slice(0, top);
-
-      console.log("Top photo processing complete" + items);
-
-      for (var i = 0; i < items.length; i++) {
-         var index = items[i][0];
-         if (media[index][2] === "video") {
-            $("#topphoto").append("<div class='media'><a target='_blank' href='" + media[index][0] + "'><video controls loop autoplay class='media' src='" + media[index][3] + "'></video></a></div>");
-         } else { 
-            $("#topphoto").append("<div class='media'><a target='_blank' href='" + media[index][0] + "'><img src='" + media[index][3] + "'></img></a></div>");
+      if (!hasRunTop) {
+         // create new 2D array with index # and likes
+         var items = []
+         for (var i = 0; i < media.length; i++) {
+            items.push([i, media[i][1]]);
          }
+         console.log(items);
+
+         // sort array by likes
+         items.sort(function(first, second) {
+             return second[1] - first[1];
+         });
+         console.log(items);
+
+         items = items.slice(0, top);
+
+         console.log("Top photo processing complete" + items);
+
+         for (var i = 0; i < items.length; i++) {
+            var index = items[i][0];
+            if (media[index][2] === "video") {
+               $("#topphoto").append("<div class='media'><a target='_blank' href='" + media[index][0] + "'><video controls loop autoplay class='media' src='" + media[index][3] + "'></video></a></div>");
+            } else { 
+               $("#topphoto").append("<div class='media'><a target='_blank' href='" + media[index][0] + "'><img src='" + media[index][3] + "'></img></a></div>");
+            }
+         }
+         hasRunTop = true;
+      } else {
+         toggleDisplay("topphoto");
       }
-      document.getElementById("showTop").style.display = "none";
       return null;
+   }
+
+   function toggleDisplay(id) {
+      // get current state
+      console.log("this component is " + document.getElementById(id).style.visibility);
+      if (document.getElementById(id).style.visibility === "hidden") {
+         document.getElementById(id).style.visibility = "visible";
+      } else {
+         document.getElementById(id).style.visibility = "hidden"
+      }
    }
    
    // gets all users followers
