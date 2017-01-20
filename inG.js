7076730848bb17751658e812579234ddc1c9c687
sunh20 <sunh20@uw.edu>
2017-01-20 11:37:29 -0800

    removed old code,  db showTop

--- inG.js
+++ inG.js
@@ -121,7 +121,7 @@
             $("#topphoto").append("<div class='media'><a target='_blank' href='" + media[index][0] + "'><img src='" + media[index][3] + "'></img></a></div>");
          }
       }
-      document.getElementById("showtop").style.display = "none";
+      document.getElementById("showTop").style.display = "none";
       return null;
    }
    
@@ -139,65 +139,4 @@
       });
       return null;
    }
-
-
-      /*
-   // gets all photos from user, shows recent and top photos
-   function getMedia() {
-      if (!hasRun) {
-         $.ajax ({  
-            type: "GET",
-            dataType: "jsonp",
-            cache: false,
-            url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + access_token,
-            success: function(data) {
-               console.log("sucessfully retrived media");
-               var totalLength = data.data.length;
-               var dict = {};
-               var recent = 5;
-               var top = 3;
-               // inserts photo index and #likes into a map, displays recent and top
-               for (var i = 0; i < totalLength; i++) {
-                  dict[i] = data.data[i].likes["count"];
-                  if (i < recent) {
-                     if (data.data[i].type === "video") {
-                        $("#recentpics").append("<div class='media'><a target='_blank' href='" + data.data[i].link + "'><video controls loop autoplay class='media' src='" + data.data[i].videos.low_resolution.url + "'></video></a></div>");
-                     } else { 
-                        $("#recentpics").append("<div class='media'><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></div>");
-                     }
-                  }
-               }
-               // Create items array, sort, slice top three
-               var items = Object.keys(dict).map(function(key) {
-                   return [key, dict[key]];
-               });
-
-               items.sort(function(first, second) {
-                   return second[1] - first[1];
-               });
-
-               items = items.slice(0, top);
-
-               console.log("Top photo processing complete" + items);
-
-               for (var i = 0; i < items.length; i++) {
-                  var index = items[i][0];
-                  if (data.data[index].type === "video") {
-                     $("#topphoto").append("<div class='media'><a target='_blank' href='" + data.data[index].link + "'><video controls loop autoplay class='media' src='" + data.data[index].videos.low_resolution.url + "'></video></a></div>");
-                  } else { 
-                     $("#topphoto").append("<div class='media'><a target='_blank' href='" + data.data[index].link + "'><img src='" + data.data[index].images.low_resolution.url + "'></img></a></div>");
-                  }
-               }
-               document.getElementById("loadMedia").style.display = 'none';
-               document.getElementById("recent").innerHTML = "Recent Photos";
-               document.getElementById("top").innerHTML = "Top " + top + " Photos";
-            }
-         });
-      }
-      hasRun = true;
-      return null;
-
-   }
-   */
-
 })();
